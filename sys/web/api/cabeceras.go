b6743521849d5004c5f086bafeb41435e65e60d1
Carlos Peña <gesaodin@gmail.com>
2020-12-02 17:14:40 -0400

    Configurando WEB Servicios 443 VI: Cambiando Cabeceras

--- sys/web/api/cabeceras.go
+++ sys/web/api/cabeceras.go
@@ -2,15 +2,17 @@ package api
 
 import "net/http"
 
+//Cabecera Conexion de archivos locales
 func Cabecera(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Requested-With")
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 }
 
+//CabeceraW Conexion de archivos Para app
 func CabeceraW(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -20,6 +22,7 @@ func CabeceraW(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 }
 
+//CabeceraRechazada Archivos de rechazos
 func CabeceraRechazada(w http.ResponseWriter, estatus int, m string) {
 	w.WriteHeader(estatus)
 	msj := []byte(m)
